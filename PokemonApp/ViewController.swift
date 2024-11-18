538119e335f2e77af2ae24a2f71695e2a69ba196
Carlos G. Angeles La Torre <carlosangeles.11.13@gmail.com>
2024-11-18 13:29:00 -0500

    initial commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Carlos Gerald Angeles La Torre on 18/11/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+        
+    }
+
+
+}
+
